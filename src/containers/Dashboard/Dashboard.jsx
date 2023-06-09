b42ba0db1cef85d826169fbcac56e4f672cf718b
SheaDevOp <nacho.dieguez18@gmail.com>
2023-06-09 10:28:25 -0300

    modify padding

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -1,9 +1,25 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const Dashboard = () => {
   return (
-    <Typography>Dashboard</Typography>
+    <Box>
+      <Typography sx={styles.pageTitle} variant="h5">Channel Dashboard</Typography>
+      <Box sx={styles.columnContainer}>
+
+      </Box>
+    </Box>
   )
+}
+
+/** @type {import("@mui/material").SxProps} */
+const styles = {
+  pageTitle: {
+    mb: 2
+  },
+  columnContainer: {
+    columns: '280px 3',
+    maxWidth: 1400
+  }
 }
 
 export default Dashboard;
